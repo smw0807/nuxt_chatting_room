ae16594d396d31ea046b1058bc60fcc272c7c8f9
smw0807 <smw0807@gmail.com>
2022-06-24 19:27:31 +0900

    아바타 표기 방식 변경

--- components/app-bar-right/avatar.vue
+++ components/app-bar-right/avatar.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <v-avatar v-if="user.image"
-      :color="avatarColor.hasImage"
+      color="white"
       size="48"
       >
       <img :src="'userProfiles/' + user.image" style="max-width:35px; max-height:35px;"/>
     </v-avatar>
 
     <v-avatar v-else
-      :color="user.gender === 'male' ? avatarColor.male : avatarColor.female"
+      color="blue lighter-4"
       size="48"
       >
       <v-icon dark >
@@ -21,15 +21,6 @@
 
 <script>
 export default {
-  data() {
-    return {
-      avatarColor: {
-        male: 'blue lighter-4',
-        female: 'pink lighten-4',
-        hasImage: 'white' //이미지가 있을 경우
-      }
-    }
-  },
   computed: {
     user() {
       return this.$store.getters['user/info'];
