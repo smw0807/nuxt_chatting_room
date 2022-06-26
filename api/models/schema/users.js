e289fabeedfae838b72bd9a75d3dc4fe43baed01
smw0807 <smw0807@gmail.com>
2022-06-26 15:32:06 +0900

    사용자 스키마 수정

--- api/models/schema/users.js
+++ api/models/schema/users.js
@@ -7,25 +7,29 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  email: {
+  email: { //이메일
     type: String,
     required: true,
     unique: true,
   },
-  password: {
+  password: { //비밀번호
     type: String,
     required: true,
   },
-  name: {
+  name: { //이름
     type: String,
     required: true,
   },
-  nickName: {
+  nickName: { //닉네임
     type: String,
     required: true,
   },
-  image: String,
-  desc: String
+  image: String, // 프로필 사진
+  desc: String, // 자기소개
+  signUpDt: { // 가입일
+    type: Date,
+    default: Date.now
+  }
 })
 
 module.exports = mongoose.models.users || mongoose.model('users', userSchema);
