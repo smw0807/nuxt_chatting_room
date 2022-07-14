a400d0f8b262540591f7966ee472e0d3d9486598
smw0807 <smw0807@gmail.com>
2022-07-14 23:21:24 +0900

    불필요 파일 제거

--- store/index.js
+++ store/index.js
@@ -1,35 +1,15 @@
 
 export const state = () => {
   return {
-    showAlert: false,
-    alertData: {
-      type: 'primary',
-      title: '',
-      text: '',
-    }
+    
   }
 }
 
 export const mutations = {
-  setAlert(state, payload) {
-    state.showAlert = payload.alert;
-    state.alertData.type = payload.type;
-    state.alertData.title = payload.title;
-    state.alertData.text = payload.text;
-  },
 }
 
 export const getters = {
-  showAlert(state) {
-    return state.showAlert;
-  },
-  alertData(state) {
-    return state.alertData;
-  },
 }
 
 export const actions = {
-  updateAlert({commit}, params) {
-    commit('setAlert', params);
-  },
 }
